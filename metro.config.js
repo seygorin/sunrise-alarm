606a3e4007dee2f76ca33cda8998feedf20f7033
Sergey <seygorin@gmail.com>
2024-09-22 19:08:53 +0500

    chore: added metro config changes

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,17 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    './src',
+    './android',
+    './ios',
+    './node_modules',
+    './.vscode',
+    './.yarn',
+  ],
+
+  watchman: true,
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
